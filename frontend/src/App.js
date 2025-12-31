f4ea7398bdc5a8eb881c5e9d0a9d8ed673996bbf
JIntorcio <John@Intorcio.org>
2025-12-31 05:35:47 +0000

    Fix paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useState, useEffect } from 'react'
-import GroceryList from './components/GroceryList'
+import GroceryList from './components/GroceryList.js'
 
 function App() {
   const [items, setItems] = useState([])
